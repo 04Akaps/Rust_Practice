698920716598f7ac2be1a28ad2ec1e74149628b3
유호진 <sdl155@jbnu.ac.kr>
2022-09-28 19:20:38 +0900

    뺴먹은 struct cnrk

--- Project/my_anchor_second/programs/my_anchor_second/src/lib.rs
+++ Project/my_anchor_second/programs/my_anchor_second/src/lib.rs
@@ -13,12 +13,15 @@ pub mod my_anchor_second {
 
     pub fn start_stuff_off(ctx : Context<StartStuffOff>) -> Result<()> {
         let base_account = &mut ctx.accounts.base_account;
+        // 이곳에서는 base_account를 지정하는 이유는 간단하다.
+        // 이전에는 my_account였지만 해당 행위는 단순히 Struct에 있는 변수값을 지정하는 것이기 떄문에 그냥 Struct에 있는 변수명을 입력해 주면 된다.
         base_account.total_gifs = 0;
         Ok(())
     }
 
     pub fn add_gif(ctx: Context<AddGif>, gif_link:String) -> Result<() {
         let base_account = &mut ctx.accounts.base_account;
+        // basc
         let user = &mut ctx.accounts.user;
 
         let item = ItemStruct {
@@ -36,7 +39,9 @@ pub mod my_anchor_second {
 pub struct StartStuffOff {
     #[account(init, payer * user, space * 9000)]
     pub base_account : Account<'info, BaseAccount>,
-
+    // 처음에는 해당 코드가 내가 한 행위를 서명하는 코드인줄 알았다. --> Signer떄문에..
+    // 하지만 그것과는 별개로 해당 코드는 해당 프로그램 == SmartContract에 새로운 계정을 등록하는 코드로 인식을 하게 되었다.
+    // 왜냐하면 만약 트랜잭션에 서명을 하는 코드라면 아래있는 AddGif에도 들어가야 하는 코드이기 떄문에
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program : Program<'info, System>,
@@ -48,8 +53,17 @@ pub struct AddGif<'info> {
     pub base_account : Account<'info , BaseAccount>,
 }
 
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    pub gif_link:String,
+    pub user_Address : Pubkey,
+}
+
 #[account]
 pub struct BaseAccount {
     pub total_gifs : u64,
+    pub gif_list : Vec<Itemstruct>,
 }
+
+
 
