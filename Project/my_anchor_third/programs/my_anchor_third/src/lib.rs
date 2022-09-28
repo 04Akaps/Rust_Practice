c55cacfc3da60c7845f97e0f3747743ae1707a77
유호진 <sdl155@jbnu.ac.kr>
2022-09-28 19:50:33 +0900

    zero account인지 확인하는 함수 작성 및 주석 작업 중

--- Project/my_anchor_third/programs/my_anchor_third/src/lib.rs
+++ Project/my_anchor_third/programs/my_anchor_third/src/lib.rs
@@ -4,6 +4,7 @@ pub mod constant;
 pub mod error;
 pub mod states;
 
+
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
@@ -17,3 +18,24 @@ pub mod my_anchor_third {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+
+pub fn is_zero_account(account_info: &AccountInfo) -> bool{
+    // 들어오는 주소값이 zero인지 체크하는 함수
+    let account_data : &[u8] = &account_info.data.borrow();
+    // AccountInfo의 data타입은 RefCel
+    // 이중 borrow데이터가 의미하는 것은 wrap되어 있는 데이터를 가져오는 것을 의미한다.
+    // -> https://doc.rust-lang.org/nightly/core/cell/struct.RefCell.html#method.borrow
+
+
+    let len = account_data.len();
+
+
+    for i in 0..len - 1{
+        if account_data[i] != 0{
+            return false;
+        }
+    };
+
+    true
+}
