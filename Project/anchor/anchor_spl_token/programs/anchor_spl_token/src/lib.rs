93259eb23d6d48d8881b89fae382907752632be1
유호진 <sdl155@jbnu.ac.kr>
2022-09-30 18:29:41 +0900

    동작 여부는 고려하지 않고 일단 구현 가능한 함수는 모두 구현 및 check 성공

--- Project/anchor/anchor_spl_token/programs/anchor_spl_token/src/lib.rs
+++ Project/anchor/anchor_spl_token/programs/anchor_spl_token/src/lib.rs
@@ -2,7 +2,7 @@ use anchor_lang::prelude::*;
 use anchor_spl::token::{
     self, approve, burn, close_account, freeze_account, initialize_account, initialize_mint,
     mint_to, revoke, set_authority, transfer, Approve, Burn, InitializeAccount, Mint, MintTo,
-    Token, TokenAccount, Transfer,
+    SetAuthority, Token, TokenAccount, Transfer, *,
 };
 
 pub mod errors;
@@ -56,11 +56,21 @@ pub mod anchor_spl_token {
         Ok(())
     }
 
-    // pub fn set_authority(ctx: Context<AuthorityStruct>) -> Result<()> {
-    //     Ok(())
-    // }
+    pub fn set_authority(
+        ctx: Context<AuthorityStruct>,
+        authority_type: AuthorityType,
+        new_authority: Pubkey,
+    ) -> Result<()> {
+        token::set_authority(
+            ctx.accounts.set_authority_ctx(),
+            authority_type.into(),
+            Some(new_authority),
+        );
+        Ok(())
+    }
 
     pub fn initialize_account(ctx: Context<InitializeStruct>) -> Result<()> {
+        token::initialize_account(ctx.accounts.initialize_account_ctx());
         Ok(())
     }
 }
@@ -133,20 +143,20 @@ pub struct InitializeStruct<'info> {
     pub token_program: Program<'info, Token>,
 }
 
-// #[derive(Accounts)]
-// pub struct AuthorityStruct<'info> {
-//     // 의문점은 mint함수에 대한 owner권한을 어떻게 설정하냐가 의문점
-//     pub sender: Signer<'info>,
+#[derive(Accounts)]
+pub struct AuthorityStruct<'info> {
+    // 의문점은 mint함수에 대한 owner권한을 어떻게 설정하냐가 의문점
+    pub sender: Signer<'info>,
 
-//     #[account(mut)]
-//     pub sender_token: Account<'info, TokenAccount>, // mint를 받을 user의 주소로 인식
+    #[account(mut)]
+    pub sender_token: Account<'info, TokenAccount>, // mint를 받을 user의 주소로 인식
 
-//     #[account(mut)]
-//     pub receiver_token: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub receiver_token: Account<'info, TokenAccount>,
 
-//     pub mint: Account<'info, Mint>,
-//     pub token_program: Program<'info, Token>,
-// }
+    pub mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+}
 
 impl<'info> MintStruct<'info> {
     fn mint_ctx(&self) -> CpiContext<'_, '_, '_, 'info, MintTo<'info>> {
@@ -215,11 +225,33 @@ impl<'info> InitializeStruct<'info> {
     }
 }
 
-// impl<'info> AuthorityStruct<'info> {
-//     fn setauthority_ctx(&self) -> CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
-//         CpiContext::new(self.token_program.to_account_info(), SetAuthority {
-//             current_authority : ,
-//             account_or_mint ,
-//         })
-//     }
-// }
+impl<'info> AuthorityStruct<'info> {
+    fn set_authority_ctx(&self) -> CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
+        CpiContext::new(
+            self.token_program.to_account_info(),
+            SetAuthority {
+                current_authority: self.sender.to_account_info(),
+                account_or_mint: self.mint.to_account_info(),
+            },
+        )
+    }
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub enum AuthorityType {
+    MintTokens,    // 새로운 토큰을 민팅할 권한
+    FreezeAccount, // 계정을 잠글 권한
+    AccountOwner,  // 주어진 토큰 주소의 오너
+    CloseAccount,  // 토큰 계정을 잠글 권한
+}
+
+impl From<AuthorityType> for spl_token::instruction::AuthorityType {
+    fn from(authority_ty: AuthorityType) -> spl_token::instruction::AuthorityType {
+        match authority_ty {
+            AuthorityType::MintTokens => spl_token::instruction::AuthorityType::MintTokens,
+            AuthorityType::FreezeAccount => spl_token::instruction::AuthorityType::FreezeAccount,
+            AuthorityType::AccountOwner => spl_token::instruction::AuthorityType::AccountOwner,
+            AuthorityType::CloseAccount => spl_token::instruction::AuthorityType::CloseAccount,
+        }
+    }
+}
