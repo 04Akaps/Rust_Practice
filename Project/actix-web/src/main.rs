b43ad161644670d4fb2ad2cac9b56733e7d6f058
유호진 <sdl155@jbnu.ac.kr>
2022-10-11 18:52:00 +0900

    actix_web작업 -> cors dependency 불러오기 및 적용 완료료

--- Project/actix-web/src/main.rs
+++ Project/actix-web/src/main.rs
@@ -1,59 +1,49 @@
-use actix_web::{ get, web, App,  HttpServer, HttpResponse};
+use actix_cors::Cors;
+use actix_web::{get, http, web, App, HttpServer};
 use std::sync::Mutex;
 
 mod api;
 mod utils;
 
-use api::get_api::{
-    manual_hello,
-};
+use api::get_api::manual_hello;
 
-use api::post_api::{
-    echo
-};
+use api::post_api::echo;
 
 struct AppStateWithCounter {
     app_name: String,
-    counter : Mutex<i32>
+    counter: Mutex<i32>,
 }
 
 #[get("/")]
-async fn start(data : web::Data<AppStateWithCounter>) -> String {
-    let mut counter = data.counter.lock().unwrap(); 
+async fn start(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap();
     let app_name = &data.app_name;
 
-    *counter += 1; 
+    *counter += 1;
 
     format!("appName is : {app_name} Request number: {counter}")
 }
 
-fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::resource("/app")
-            .route(web::get().to(|| async { HttpResponse::Ok().body("app") }))
-            .route(web::head().to(HttpResponse::MethodNotAllowed)),
-    );
-    // router를 이렇게도 작성이 가능합니다.
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
     let status = web::Data::new(AppStateWithCounter {
-        app_name : "my first rust server".to_string(),
-        counter : Mutex::new(0),
+        app_name: "my first rust server".to_string(),
+        counter: Mutex::new(0),
     });
 
     HttpServer::new(move || {
+        let cors = Cors::default()
+            .allowed_origin("https://www.rust-lang.org/")
+            .allowed_methods(vec!["GET,POST,DELETE"])
+            .max_age(3600);
+
         App::new()
-        .app_data(status.clone()) 
-        .configure(config)
-        .service(start)
-        .service(web::scope("/app").service(echo).service(manual_hello))
+            .wrap(cors)
+            .app_data(status.clone())
+            .service(start)
+            .service(web::scope("/app").service(echo).service(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
-
-
 }
